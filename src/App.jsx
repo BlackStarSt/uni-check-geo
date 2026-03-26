a4b1c6782e681b2dd6dd067de8f97a09c222fa2d
BlackStarSt <sthefanya86@gmail.com>
2026-03-26 19:36:44 -0300

    Estrutura limpa básica inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>UniCheckGeo</h1>
+      {/* Aqui vai entrar o seu botão de presença e o mapa depois */}
+    </>
+  )
+}
+
+export default App
