531bc4245c1f24fa1eda39b15a610ce141155a1c
BlackStarSt <sthefanya86@gmail.com>
2026-04-09 23:30:45 -0300

    feat: Adição da logica de perfil e captura de valores dos campos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 import '../styles/Login.css';
 
-import LogoImg from '../assets/logo_horiz.jpg';
+import LogoImg from '../assets/logo.png';
+import password_icon from '../assets/icons/password_icon.png';
+import user_icon from '../assets/icons/user_icon.png';
+import perfil_icon from '../assets/icons/perfil_icon.png'
 
 import InputLogin from '../components/InputLogin.jsx';
+import SelectLogin from '../components/selectLogin.jsx';
 
 function Login() {
     const handleLogin = (e) => {
@@ -10,25 +16,32 @@ function Login() {
         alert("Login clicado!");
     };
 
+    const [matricula, setMatricula] = useState('');
+    const [senha, setSenha] = useState('');
+    const [perfil, setPerfil] = useState('aluno');
+
+    const opcoesPerfil = [
+        { value: 'aluno', label: 'Aluno' },
+        { value: 'coordenador', label: 'Coordenador' },
+        { value: 'admin', label: 'Admin' }
+    ];
+
     return (
         <div className="login-container">
-            <div className="fig-container">
-                <div className="login-fig-1"></div>
-                <div className="login-fig-2"></div>
+            <img src={LogoImg} className='logo-img' />
+            <div className="login-title">
+                <h2 className="title">Bem-vindo à sua</h2>
+                <h2 className="title">Universidade</h2>
             </div>
             <form onSubmit={handleLogin}>
-                <img src={LogoImg} className='logo-img' />
-                <InputLogin type="text" placeholder="Matrícula" />
-                <InputLogin type="password" placeholder="Senha" />
-
+                <InputLogin type="text" placeholder="Matrícula Institucional" icon={user_icon} value={matricula} onChange={(e) => setMatricula(e.target.value)}  />
+                <InputLogin type="password" placeholder="Senha" icon={password_icon} value={senha} onChange={(e) => setSenha(e.target.value)}  />
+                <SelectLogin label="Perfil" id="userPerfil" name="userPerfil" icon={perfil_icon} options={opcoesPerfil} value={perfil} onChange={(e) => setPerfil(e.target.value)} />
                 <button type="submit" className="btn-login">
                     Entrar
                 </button>
             </form>
-            <div className="fig-container">
-                <div className="login-fig-2"></div>
-                <div className="login-fig-1"></div>
-            </div>
+            <a href="#" className="login-link"><p>Esqueci minha senha</p></a>
         </div>
     );
 }
