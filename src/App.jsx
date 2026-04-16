cddd85ed30ca729371f799906a1aef69158eeef5
BlackStarSt <sthefanya86@gmail.com>
2026-04-16 17:52:08 -0300

    feat: Adição do caminho para a página de check in

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Home from './pages/Home';
+import CheckIn from './pages/CheckIn';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/check-in' element={<CheckIn />} />
       </Routes>
     </BrowserRouter>
   )
